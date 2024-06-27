eebf508ce34745a3473cc34e275fb26ebca41dac
ManoMax <gabrielmaxcontato@gmail.com>
2024-06-27 17:40:36 -0300

    feat: coupon session

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -31,16 +31,17 @@ import * as $$$$$$14 from "./sections/Links/LinkTree.tsx";
 import * as $$$$$$15 from "./sections/Miscellaneous/CampaignTimer.tsx";
 import * as $$$$$$16 from "./sections/Miscellaneous/CookieConsent.tsx";
 import * as $$$$$$17 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$18 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$19 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$20 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$21 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$22 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$23 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$24 from "./sections/Session.tsx";
-import * as $$$$$$25 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$26 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$27 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$18 from "./sections/Product/Coupon.tsx";
+import * as $$$$$$19 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$20 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$21 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$22 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$23 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$24 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$25 from "./sections/Session.tsx";
+import * as $$$$$$26 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$27 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$28 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -69,16 +70,17 @@ const manifest = {
     "site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$15,
     "site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$16,
     "site/sections/Newsletter/Newsletter.tsx": $$$$$$17,
-    "site/sections/Product/ProductDetails.tsx": $$$$$$18,
-    "site/sections/Product/ProductShelf.tsx": $$$$$$19,
-    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$20,
-    "site/sections/Product/SearchResult.tsx": $$$$$$21,
-    "site/sections/Product/ShelfWithImage.tsx": $$$$$$22,
-    "site/sections/Product/Wishlist.tsx": $$$$$$23,
-    "site/sections/Session.tsx": $$$$$$24,
-    "site/sections/Social/InstagramPosts.tsx": $$$$$$25,
-    "site/sections/Social/WhatsApp.tsx": $$$$$$26,
-    "site/sections/Theme/Theme.tsx": $$$$$$27,
+    "site/sections/Product/Coupon.tsx": $$$$$$18,
+    "site/sections/Product/ProductDetails.tsx": $$$$$$19,
+    "site/sections/Product/ProductShelf.tsx": $$$$$$20,
+    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$21,
+    "site/sections/Product/SearchResult.tsx": $$$$$$22,
+    "site/sections/Product/ShelfWithImage.tsx": $$$$$$23,
+    "site/sections/Product/Wishlist.tsx": $$$$$$24,
+    "site/sections/Session.tsx": $$$$$$25,
+    "site/sections/Social/InstagramPosts.tsx": $$$$$$26,
+    "site/sections/Social/WhatsApp.tsx": $$$$$$27,
+    "site/sections/Theme/Theme.tsx": $$$$$$28,
   },
   "actions": {
     "site/actions/minicart/submit.ts": $$$$$$$$$0,
---
+++ sections/Product/Coupon.tsx
@@ -0,0 +1,17 @@
+export interface Props {
+    code: string;
+    description: string;
+}
+
+export default function Coupon({ code, description }: Props) {
+    return (
+        <div class="bg-white p-6 rounded-lg shadow-md max-w-sm mx-auto">
+        <h2 class="text-lg font-bold mb-2">Desconto Disponível!</h2>
+        <div class="flex items-center justify-between bg-gray-100 p-4 rounded">
+            <span class="text-gray-700">Código:</span>
+            <span class="bg-blue-500 text-white px-3 py-1 rounded">{ code }</span>
+        </div>
+        <p class="text-gray-600 mt-4">{ description }</p>
+        </div>
+    );
+}
